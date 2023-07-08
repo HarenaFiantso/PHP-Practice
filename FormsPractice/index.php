510915ca4adb3f66b66fd63910d35022c1c3dfe8
Fiantso Harena <fiantsorav@gmail.com>
2023-07-08 10:40:30 +0300

    Feat: Create an exercice about send files with a script php and an HTML forms

---
+++ FormsPractice/index.php
@@ -0,0 +1,28 @@
+<?php
+    /*
+        PRATIQUE D'ENVOIE D'UN FICHIER AVEC PHP
+        Le but de cette exercice pratique est de pratique, bien évidemment, l'envoi des fichiers avec PHP à l'aide 
+        d'une formulaire.
+    */
+
+    // Envoie du fichier avec un script PHP
+    if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+        if ($_FILES['image']['size'] <= 3000000) {
+            $informationImage = pathinfo($_FILES['image']['name']);
+            $extensionImage = $informationImage['extension'];
+            $extensionArray = array('png', 'gif', 'jpg', 'jpeg');
+
+            if (in_Array($extensionImage, $extensionArray)) {
+                move_uploaded_file($_FILES['image']['tmp_name'], 
+                'uploads/'. time(). rand().rand().$extensionImage);
+                echo "Envoie bien réussi ! ";
+            }
+        }
+    }
+
+    echo '<form method="post" action="index.php" enctype="multipart/form-data">
+            <h1>Formulaire</h1>
+            <input type="file" name="image"/> <br />
+            <button type="submit">Envoyer</button>
+        </form>';
+?>
